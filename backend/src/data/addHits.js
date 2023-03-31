e1aeaa2a7b3c0a3df510998403cec6f5f03a3af9
mrpirated <deepeshrathi9@gmail.com>
2023-03-31 22:41:22 +0530

    fixed add hits

--- backend/src/data/addHits.js
+++ backend/src/data/addHits.js
@@ -5,11 +5,15 @@ const debug = dbg("data:addHits");
 
 const addHits = async () => {
 	return new Promise((resolve, reject) => {
-		pool.query(`UPDATE hits ET views=views+1`, (err, result) => {
-			if (err) {
-				reject({ success: false, message: err });
-			} else resolve({ success: true, message: "Views updated successfully" });
-		});
+		pool.query(
+			`UPDATE hits SET views=views+1 WHERE type = "website"`,
+			(err, result) => {
+				if (err) {
+					reject({ success: false, message: err });
+				} else
+					resolve({ success: true, message: "Views updated successfully" });
+			}
+		);
 	});
 };
 export default addHits;
