f6802ead39ac91b60aae69e6f5558583bf145e02
Cyber_Infernox <sayonkar@gmail.com>
2023-04-01 15:52:58 +0530

    Changed image size

--- frontend/src/Components/Events/Team.jsx
+++ frontend/src/Components/Events/Team.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import teamlogo from "./Assests/teamlogo.webp";
+import "./Team.css";
 
 function Team(props) {
-	const { name, contact, imageUrl } = props;
-	return (
-		<div className='m-10 lg:mx-20'>
-			<div className='w-full h-full flex flex-col justify-center items-center'>
-				<img
-					src={imageUrl}
-					alt='avatar'
-					className='rounded-full cover h-[150px] w-[150px] lg:h-[200px] lg:w-[200px]'
-				/>
-				<p className='font-bold text-xl lg:text-2xl mt-4'>
-					{name.toUpperCase()}
-				</p>
-				<p className='font-normal lg:text-md'>{contact}</p>
-			</div>
-		</div>
-	);
+  const { name, contact, imageUrl } = props;
+  return (
+    <div className="m-10 lg:mx-20">
+      <div className="w-full h-full flex flex-col justify-center items-center">
+        <div className="imgBx">
+          <img src={imageUrl} alt="avatar" className="img" />
+        </div>
+        <p className="font-bold text-xl lg:text-2xl mt-4">
+          {name.toUpperCase()}
+        </p>
+        <p className="font-normal lg:text-md">{contact}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Team;
