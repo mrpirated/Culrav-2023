bb478bba08f59e9290b0c2f9bc0cb57ded81afda
mrpirated <deepeshrathi9@gmail.com>
2023-04-02 14:05:51 +0530

    added filter

--- frontend/src/Pages/Dashboard/EditEC.jsx
+++ frontend/src/Pages/Dashboard/EditEC.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import AdminDataList from "./AdminDataList";
 import AddEc from "./AddEc";
+import { useSelector } from "react-redux";
 function EditEC(props) {
+	const auth = useSelector((state) => state.auth);
 	const { ecs, setRefreshList, commitee, commiteeEvents } = props;
 	return (
 		<div className='flex flex-col md:flex-row w-full '>
 			<div className='w-full mt-4 p-2'>
-				<AdminDataList type={"ec"} setRefreshList={setRefreshList} data={ecs} />
+				<AdminDataList
+					type={"ec"}
+					setRefreshList={setRefreshList}
+					data={
+						auth.user.type == "FS" || auth.user.type == "ADMIN"
+							? ecs
+							: ecs.filter((e) => auth.poc.includes(e.commitee_id))
+					}
+				/>
 			</div>
 			<div className='w-full mt-4 p-2 md:w-[80%] md:mx-4'>
 				<AddEc
