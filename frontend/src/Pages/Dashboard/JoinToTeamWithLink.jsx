138b26dae76ad490d64509157af72fb9ccf75cdc
mrpirated <deepeshrathi9@gmail.com>
2023-04-05 03:15:49 +0530

    updated team list after joining with code

--- frontend/src/Pages/Dashboard/JoinToTeamWithLink.jsx
+++ frontend/src/Pages/Dashboard/JoinToTeamWithLink.jsx
@@ -3,39 +3,47 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import addMemberToTeamLinkAPI from "../../api/addMemberToTeamLinkAPI";
 import { toast } from "react-hot-toast";
-import { setLoading } from "../../store/auth";
-
+import { setLoading, setTeams } from "../../store/auth";
+import getUserTeamsAPI from "../../api/getUserTeamsAPI";
 const JoinToTeamWithLink = () => {
-  const auth = useSelector((state) => state.auth);
-  const token = auth.token;
-  const { link } = useParams();
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+	const auth = useSelector((state) => state.auth);
+	const token = auth.token;
+	const { link } = useParams();
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (token != "") {
-      dispatch(setLoading({ loading: true }));
-      addMemberToTeamLinkAPI({
-        token: token,
-        link: link,
-      }).then((response) => {
-        dispatch(setLoading({ loading: false }));
-        if (response.success) {
-          toast.success(response.message);
-          navigate("/dashboard");
-        } else {
-          toast.error(response.message);
-          navigate("/");
-        }
-      });
-    }
-  }, [token]);
+	useEffect(() => {
+		if (token != "") {
+			dispatch(setLoading({ loading: true }));
+			addMemberToTeamLinkAPI({
+				token: token,
+				link: link,
+			})
+				.then((response) => {
+					if (response.success) {
+						toast.success(response.message);
+						navigate("/dashboard");
+						return getUserTeamsAPI({ token: auth.token });
+					} else {
+						toast.error(response.message);
+						navigate("/");
+						return Promise.reject(response);
+					}
+				})
+				.then((response) => {
+					dispatch(setTeams({ teams: response.data }));
+				})
+				.finally(() => {
+					dispatch(setLoading({ loading: false }));
+				});
+		}
+	}, [token]);
 
-  return (
-    <>
-      <div></div>
-    </>
-  );
+	return (
+		<>
+			<div></div>
+		</>
+	);
 };
 
 export default JoinToTeamWithLink;
