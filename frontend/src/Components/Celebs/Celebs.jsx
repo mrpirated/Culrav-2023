fa5df2f093c942bdda2eac07a4a9ae9ed74a21ea
mrpirated <deepeshrathi9@gmail.com>
2023-04-11 23:07:48 +0530

    added images to server

--- frontend/src/Components/Celebs/Celebs.jsx
+++ frontend/src/Components/Celebs/Celebs.jsx
@@ -3,61 +3,61 @@ import Now from "./Now/Now";
 import Past from "./Past/Past";
 
 const Celebs = () => {
-  const [set, setState] = useState(true);
-  const animateRef = useRef();
+	const [set, setState] = useState(true);
+	const animateRef = useRef();
 
-  useEffect(() => {
-    const applyContainerProperties = () => {
-      animateRef.current.classList.add("effect-container");
-    };
+	useEffect(() => {
+		const applyContainerProperties = () => {
+			animateRef.current.classList.add("effect-container");
+		};
 
-    const onClick = () => {
-      setTimeout(() => {
-        animateRef.current.classList.remove("activeCeleb");
-      }, 500);
-      animateRef.current.classList.add("activeCeleb");
-    };
+		const onClick = () => {
+			setTimeout(() => {
+				animateRef.current.classList.remove("activeCeleb");
+			}, 500);
+			animateRef.current.classList.add("activeCeleb");
+		};
 
-    applyContainerProperties();
+		applyContainerProperties();
 
-    animateRef.current.addEventListener("mouseup", onClick);
+		animateRef.current.addEventListener("mouseup", onClick);
 
-    const cleanupRef = animateRef.current;
+		const cleanupRef = animateRef.current;
 
-    return () => {
-      cleanupRef.removeEventListener("mouseup", onClick);
-    };
-  });
+		return () => {
+			cleanupRef.removeEventListener("mouseup", onClick);
+		};
+	});
 
-  return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className="text-darker mt-[70px] text-center pt-[35px] pb-[28px] text-[40px] font-bold tracking-wide japanFont">
-        CELEBS
-      </h1>
-      <div ref={animateRef} className="relative text-center mt-[15px]">
-        <span
-          onClick={() => setState(false)}
-          id="space"
-          className="text-darker mr-[200px] cursor-pointer text-[20px] transition ease-in-out duration-500"
-        >
-          PAST
-        </span>
-        <span
-          onClick={() => setState(true)}
-          className="text-darker cursor-pointer text-[20px] transition ease-in-out duration-500"
-        >
-          NOW
-        </span>
-        <div id="bar" className={set === true ? "flex justify-end" : ""}>
-          <div className="bg-black w-[70px] mt-[-2px] mr-[0px] h-[2px] mb-[40px]"></div>
-        </div>
-      </div>
-      <div className="mx-[50px] lg:mx-[100px] xl:mx-[150px]">
-        {set === true ? <Now /> : <Past />}
-        {/* <Past /> */}
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-col justify-center items-center'>
+			<h1 className='text-darker mt-[70px] text-center pt-[35px] pb-[28px] text-[40px] font-bold tracking-wide japanFont'>
+				CELEBS
+			</h1>
+			<div ref={animateRef} className='relative text-center mt-[15px]'>
+				<span
+					onClick={() => setState(true)}
+					id='space'
+					className='text-darker mr-[200px] cursor-pointer text-[20px] transition ease-in-out duration-500'
+				>
+					NOW
+				</span>
+				<span
+					onClick={() => setState(false)}
+					className='text-darker cursor-pointer text-[20px] transition ease-in-out duration-500'
+				>
+					PAST
+				</span>
+				<div id='bar' className={set === false ? "flex justify-end" : ""}>
+					<div className='bg-black w-[70px] mt-[-2px] mr-[0px] h-[2px] mb-[40px]'></div>
+				</div>
+			</div>
+			<div className='mx-[50px] lg:mx-[100px] xl:mx-[150px]'>
+				{set === true ? <Now /> : <Past />}
+				{/* <Past /> */}
+			</div>
+		</div>
+	);
 };
 
 export default Celebs;
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -2,41 +2,47 @@ import React from "react";
 import "./Home.css";
 import { motion } from "framer-motion";
 import Timer from "./Timer";
-import dristhiLogo from "../Sponsors/images/drishti.png";
 
 function Header() {
-  return (
-    <div className="header__background-image" id="home">
-      <div className="header__center">
-        <motion.div
-          className="w-full flex justify-center"
-          whileInView={{ y: [150, 0], opacity: [0, 1] }}
-          transition={{ duration: 5, ease: "easeOut" }}
-          whileTap={{ scale: 0.9 }}
-          viewport={{ once: true }}
-        >
-          <img src={dristhiLogo} className="w-[40%] md:w-[20%]" />
-        </motion.div>
-        <motion.div
-          whileInView={{ y: [150, 0], opacity: [0, 1] }}
-          transition={{ duration: 5, ease: "easeOut" }}
-          whileTap={{ scale: 0.9 }}
-          viewport={{ once: true }}
-        >
-          <h1 id="inner">CULRAV</h1>
-          <h1 id="inner-head">2023</h1>
-          {/* <div className="flex justify-center"> */}
-          <Timer />
-          {/* </div> */}
-        </motion.div>
-        <div className="scrolldown">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='header__background-image' id='home'>
+			<div className='header__center'>
+				<motion.div
+					className='flex flex-col items-center justify-center'
+					whileInView={{ y: [150, 0], opacity: [0, 1] }}
+					transition={{ duration: 5, ease: "easeOut" }}
+					whileTap={{ scale: 0.9 }}
+					viewport={{ once: true }}
+				>
+					<a
+						href='https://www.drishtiias.com/'
+						target='_blank'
+						className='flex flex-col items-center justify-center w-[40%] md:w-[20%]'
+					>
+						<img src={`https://images.culrav.online/drishti.png`} />
+					</a>
+					<span className=''>PRESENTS</span>
+				</motion.div>
+				<motion.div
+					whileInView={{ y: [150, 0], opacity: [0, 1] }}
+					transition={{ duration: 5, ease: "easeOut" }}
+					whileTap={{ scale: 0.9 }}
+					viewport={{ once: true }}
+				>
+					<h1 id='inner'>CULRAV</h1>
+					<h1 id='inner-head'>2023</h1>
+					{/* <div className="flex justify-center"> */}
+					{/* <Timer /> */}
+					{/* </div> */}
+				</motion.div>
+				<div className='scrolldown'>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
